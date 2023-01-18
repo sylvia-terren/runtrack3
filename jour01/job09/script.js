1a898d6afba928ba2c32c9efac770c12ccb90472
Sylvia <sylvia.terren@laplateforme.io>
2023-01-18 12:12:23 +0100

    Mise à jour job09

--- jour01/job09/script.js
+++ jour01/job09/script.js
@@ -1,5 +1,27 @@
 numbers = [41, 67, 27, 10, 01];
+order = 'asc'; //ou desc
 
 function tri (numbers, order) {
 
+
+     console.log(numbers)
+
+}
+
+function sort() {
+    if (order = 'asc') {
+        // numbers.sort(function (first, second) {
+        //     if (first > second) {
+        //        return -1;
+        //     }
+        //     if (first < second) {
+        //        return 1;
+        //     }
+        //     return 0;
+        //  });
+
+    }
+    else if (order = 'desc') {
+
+    }
 }
