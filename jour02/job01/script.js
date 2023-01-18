be237eed889a1539ad82d21f7ce15d472c6e2323
Sylvia <sylvia.terren@laplateforme.io>
2023-01-18 12:59:44 +0100

    Création des index.php et script.js

---
+++ jour02/job01/script.js
@@ -0,0 +1,11 @@
+var affichage = document.getElementById('citation');
+
+function citation(affichage) {
+    if(document.getElementById('button').clicked == true)
+{
+   console.log(affichage);
+}
+
+    }
+
+citation(affichage);
