9bfc1f4de96c23fdf650a72ca0deb4552b91c00a
Sylvia <sylvia.terren@laplateforme.io>
2023-01-24 11:10:51 +0100

    Création des dossiers du jour 6, des fichiers et début du Job01

---
+++ jour06/job01/script.js
@@ -0,0 +1,10 @@
+link = 
+$(document).ready(function(){
+    if ( $(window).width() > 1680 && $(window).width() < 1920) {
+       link.href = "style1.css";
+    } else if ($(window).width() > 1280 && $(window).width() < 1680 ){
+      
+    } else if ($(window).width() < 1280) {
+
+    }
+});
